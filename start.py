8eba3d3c9ad30957e631eb2c7be0af468649eb01
yueye <yueyezhimen@qq.com>
2023-04-23 15:22:24 +0800

    11

--- start.py
+++ start.py
@@ -1,12 +1,23 @@
 import sys
 import train
-
-
-def main():
+import detect
+def _detect():
+    opt = train.parse_opt();
+    opt.weights = ""
+    train.main(opt)
+def _train():
+    opt = train.parse_opt();
+    opt.data = "VOC.yaml";
+    opt.weights = ""
+    train.main(opt)
+def _resume():
     opt = train.parse_opt();
     opt.resume = True;
     opt.project = "runs/train"
     train.main(opt)
+
+def main():
+    _train()
 
 
 if __name__ == '__main__':
